07a4f966d74a204236a1d3fd6c55fd3ba1f71596
bkrigel <benjamin.krigel@gmail.com>
2018-05-14 15:47:13 -0400

    This lab was not easy.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(attendees)
+  attendees.map { |attendee| badge_maker(attendee) }
+end
+
+
+def assign_rooms(attendees)
+  attendees.each_with_index.collect do |attendee, i|
+     "Hello, #{attendee}! You'll be assigned to room #{i+1}!"
+  end
+end
+
+
+def printer(attendees)
+    batch_badge_creator(attendees).each{|words| puts words}
+    assign_rooms(attendees).each{|words| puts words}
+end
